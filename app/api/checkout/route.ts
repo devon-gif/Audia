589e8ee13ac793b917970f91b19c8ef7ac6dc014
devon-gif <devon@jobghost.ai>
2026-05-08 15:16:27 -0600

    Update route.ts

--- app/api/checkout/route.ts
+++ app/api/checkout/route.ts
@@ -8,7 +8,7 @@ import { NextResponse } from "next/server";
 import Stripe from "stripe";
 
 const stripe = new Stripe(process.env.STRIPE_SECRET_KEY || "", {
-  apiVersion: "2024-12-18.acacia",
+  apiVersion: "2026-04-22.dahlia",
 });
 
 export async function POST(req: Request) {

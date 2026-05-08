34a7078052839423b3a3a628a503d99ea8db7407
devon-gif <devon@jobghost.ai>
2026-05-08 16:04:26 -0600

    f

--- app/dashboard/layout.tsx
+++ app/dashboard/layout.tsx
@@ -1,6 +1,12 @@
 import { PlayerProvider } from "@/contexts/PlayerContext";
+import StereoPlayer from "@/app/components/dashboard/GlobalPlayer";
 import type { ReactNode } from "react";
 
 export default function DashboardLayout({ children }: { children: ReactNode }) {
-  return <PlayerProvider>{children}</PlayerProvider>;
+  return (
+    <PlayerProvider>
+      {children}
+      <StereoPlayer />
+    </PlayerProvider>
+  );
 }
